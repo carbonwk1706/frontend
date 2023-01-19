40d0a2005716791618205673e42d69fea7abc840
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-01-19 16:40:27 +0700

    fix login and set delay when login success

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,10 +2,26 @@
   <AuthLogin>
     <div>
       <v-form @submit.prevent="login">
-        <v-text-field label="Username" v-model="form.username" type="email"/>
-        <v-text-field label="Password" v-model="form.password" type="password" />
-        <v-btn type="submit" v-if="!this.$store.getters['auth/isLogin']">Log In</v-btn>
+        <v-text-field label="Username" v-model="form.username" type="email" />
+        <v-text-field
+          label="Password"
+          v-model="form.password"
+          type="password"
+        />
+        <v-btn type="submit" v-if="!this.$store.getters['auth/isLogin']"
+          >Log In</v-btn
+        >
       </v-form>
+
+      <div class="center-loading">
+        <v-progress-circular
+          v-if="loading"
+          :size="70"
+          :width="7"
+          indeterminate
+          color="primary"
+        ></v-progress-circular>
+      </div>
 
       <v-dialog v-model="error" max-width="290">
         <v-card>
@@ -22,24 +38,25 @@
 <script>
 import api from "../services/api";
 import router from "../router";
-import AuthLogin from '../components/AuthLogin.vue';
+import AuthLogin from "../components/AuthLogin.vue";
 export default {
   components: {
-    AuthLogin
+    AuthLogin,
   },
   data() {
     return {
       form: {
         username: "",
-        password: ""
+        password: "",
       },
       loading: false,
-      error: false
-    }
+      error: false,
+    };
   },
   methods: {
-    async login(){
+    async login() {
       try {
+        this.loading = true;
         const res = await api.post("/auth/login", {
           username: this.form.username,
           password: this.form.password,
@@ -48,15 +65,26 @@ export default {
         const token = res.data.token;
         localStorage.setItem("token", token);
         localStorage.setItem("user", JSON.stringify(user));
-        this.$store.dispatch('auth/logout',user)
-        router.push("/");
+        this.$store.dispatch("auth/login", user);
+        if(res.status !== 404){
+          setTimeout(() => {
+          router.push("/");
+          this.loading = false
+        },2000)
+        }
       } catch (e) {
+        this.loading = false
         this.error = true;
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <style scoped>
-
+.center-loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
 </style>
