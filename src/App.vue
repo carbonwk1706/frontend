7278e05d5afa54e76cabb3c01c3a190dea0adf7e
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-01-19 11:37:36 +0700

    hide button login when login success

--- src/App.vue
+++ src/App.vue
@@ -25,17 +25,7 @@ export default {
   
 };
 </script>
-<style scoped>
-
-.header_action {
-  border-radius: 10px;
-  background-color: rgba(0, 0, 0, 0.12);
-  margin-right: 1rem;
-}
-.app_bar {
-  background-image: linear-gradient(#00af70, #37c13d);
-  color: #ffff;
-}
+<style>
 
 
 </style>
--- src/components/HeaderNavbar.vue
+++ src/components/HeaderNavbar.vue
@@ -1,9 +1,10 @@
 <template>
   <v-app-bar class="app_bar" color="primary" dark>
-    <v-btn class="header_action" color="white">
+    <v-btn class="header_action" color="white" v-if="!isLogin">
       <v-icon class="mr-2">mdi-login</v-icon>
       LOGIN / REGISTER
     </v-btn>
+    <span class="ml-3" v-if="isLogin">LOGIN สำเร็จ</span>
     <v-spacer></v-spacer>
     <v-toolbar-title class="ml-3 text-center">EBOOK</v-toolbar-title>
     <v-spacer></v-spacer>
@@ -15,13 +16,35 @@
       <v-icon class="mr-2">mdi-cart</v-icon>
       Cart
     </v-btn>
+    <v-btn class="header_action" color="white" v-if="isLogin" @click="logout">
+      <v-icon class="mr-2">mdi-logout</v-icon>
+      Logout
+    </v-btn>
   </v-app-bar>
 </template>
 <script>
 export default {
-  
+  methods: {
+    logout(){
+      this.$store.dispatch('auth/logout')
+    }
+  },
+  computed: {
+    isLogin(){
+      return this.$store.getters['auth/isLogin']
+    }
+  }
 }
 </script>
-<style>
+<style scoped>
   
+.header_action {
+  border-radius: 10px;
+  background-color: rgba(0, 0, 0, 0.12);
+  margin-right: 1rem;
+}
+.app_bar {
+  background-image: linear-gradient(#00af70, #37c13d);
+  color: #ffff;
+}
 </style>
