40664cc2e2f78169d0c1ee66d2dc3918581fb085
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-01-19 12:13:22 +0700

    check when login success can't go back login

---
+++ src/components/AuthLogin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    isLogin() {
+      return this.$store.getters["auth/isLogin"];
+    },
+  },
+  mounted() {
+    if (this.isLogin) {
+      this.$router.push("/");
+    }
+  },
+};
+</script>
+<style></style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,18 @@
 <template>
-<v-form @submit.prevent="login">
-  <v-text-field label="Username" v-model="form.username" type="email"/>
-  <v-text-field label="Password" v-model="form.password" type="password" />
-  <v-btn type="submit" v-if="!this.$store.getters['auth/isLogin']">Log In</v-btn>
-</v-form>
+  <AuthLogin>
+    <v-form @submit.prevent="login">
+      <v-text-field label="Username" v-model="form.username" type="email"/>
+      <v-text-field label="Password" v-model="form.password" type="password" />
+      <v-btn type="submit" v-if="!this.$store.getters['auth/isLogin']">Log In</v-btn>
+    </v-form>
+  </AuthLogin>
 </template>
 <script>
+import AuthLogin from '../components/AuthLogin.vue';
 export default {
+  components: {
+    AuthLogin
+  },
   data() {
     return {
       form: {
