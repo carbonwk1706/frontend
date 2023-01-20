0506836d57f960b14a4587af2120bb6a395d3093
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-01-20 11:56:42 +0700

    fix css center

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-app-bar class="app_bar">
+  <v-toolbar class="app_bar">
     <v-btn
-      class="header_action"
+      class="px-2 mx-2 v-btn--outline header_action"
       color="white"
       v-if="!isLogin"
       @click="goToLogin"
@@ -9,52 +9,48 @@
       <v-icon class="mr-2">mdi-login</v-icon>
       ล็อกอินเข้าสู่ระบบ / สมัครสมาชิก
     </v-btn>
-    <div v-if="isLogin" class="ml-5">
-      <v-row>
-        <v-menu>
-          <template v-slot:activator="{ props }">
-            <v-btn class="header_action" v-bind="props">
-              <v-icon class="mr-2">mdi-login</v-icon>
-              สวัสดี {{ getName() }}
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-text>
-              <div class="mx-auto text-center">
-                <v-avatar color="brown">
-                  <span class="text-h5">{{ user.initials }}</span>
-                </v-avatar>
-                <h3>{{ user.fullName }}</h3>
-                <p class="text-caption mt-1">
-                  {{ user.email }}
-                </p>
-                <v-divider class="my-3"></v-divider>
-                <v-btn rounded variant="text"> Edit Account </v-btn>
-                <v-divider class="my-3"></v-divider>
-                <v-btn rounded variant="text"> Disconnect </v-btn>
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-menu>
-      </v-row>
-    </div>
+    <v-menu v-if="isLogin" offset-y>
+      <template v-slot:activator="{ props }">
+        <v-btn class="header_action" v-bind="props">
+          <v-icon class="mr-2">mdi-login</v-icon>
+          สวัสดี {{ getName() }}
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-text>
+          <div class="mx-auto text-center">
+            <v-avatar color="brown">
+              <span class="text-h5">{{ user.initials }}</span>
+            </v-avatar>
+            <h3>{{ user.fullName }}</h3>
+            <p class="text-caption mt-1">
+              {{ user.email }}
+            </p>
+            <v-divider class="my-3"></v-divider>
+            <v-btn rounded variant="text"> Edit Account </v-btn>
+            <v-divider class="my-3"></v-divider>
+            <v-btn rounded variant="text"> Disconnect </v-btn>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-menu>
     <v-spacer></v-spacer>
-    <v-toolbar-title class="align-center text-center">EBOOK</v-toolbar-title>
+    <v-toolbar-title class="text-center">EBOOK</v-toolbar-title>
 
     <v-spacer></v-spacer>
-    <v-btn class="header_action" color="white" @click="goToWishlist">
+    <v-btn class="header_action px-2 mx-2 v-btn--outline" color="white" @click="goToWishlist">
       <v-icon class="mr-2">mdi-heart</v-icon>
       รายการโปรด
     </v-btn>
-    <v-btn class="header_action" color="white" @click="goToCart">
+    <v-btn class="header_action px-2 mx-2 v-btn--outline" color="white" @click="goToCart">
       <v-icon class="mr-2">mdi-cart</v-icon>
       ตะกร้าสินค้า
     </v-btn>
-    <v-btn class="header_action" color="white" v-if="isLogin" @click="logout">
+    <v-btn class="header_action px-2 mx-2 v-btn--outline" color="white" v-if="isLogin" @click="logout">
       <v-icon class="mr-2">mdi-logout</v-icon>
       Logout
     </v-btn>
-  </v-app-bar>
+  </v-toolbar>
 </template>
 <script>
 import router from "../router";
@@ -94,7 +90,6 @@ export default {
 .header_action {
   border-radius: 10px;
   background-color: rgba(0, 0, 0, 0.12);
-  margin-right: 1rem;
 }
 .app_bar {
   background-image: linear-gradient(#00af70, #37c13d);
