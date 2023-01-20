475912df9c5cf86339e1b77e5afc87953cefd4fc
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-01-20 11:10:07 +0700

    fix profile dropdown

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,36 +9,58 @@
       <v-icon class="mr-2">mdi-login</v-icon>
       ล็อกอินเข้าสู่ระบบ / สมัครสมาชิก
     </v-btn>
-    <v-container v-if="isLogin">
-      <v-row >
-        <v-menu bottom min-width="200px" rounded offset-y>
-          <template v-slot:activator="{ on }">
-            <v-btn icon x-large v-on="on">
-              <v-avatar color="brown" size="48">
-                <span class="white--text text-h5">{{ user.initials }}</span>
+    <v-container
+  >
+    <v-row justify="start">
+      <v-menu
+        min-width="200px"
+        rounded
+      >
+        <template v-slot:activator="{ props }">
+          <v-btn
+            icon
+            v-bind="props"
+          >
+            <v-avatar
+              color="brown"
+              size="large"
+            >
+              <span class="text-h5">{{ user.initials }}</span>
+            </v-avatar>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-text>
+            <div class="mx-auto text-center">
+              <v-avatar
+                color="brown"
+              >
+                <span class="text-h5">{{ user.initials }}</span>
               </v-avatar>
-            </v-btn>
-          </template>
-          <v-card>
-            <v-list-item-content class="justify-center">
-              <div class="mx-auto text-center">
-                <v-avatar color="brown">
-                  <span class="white--text text-h5">{{ user.initials }}</span>
-                </v-avatar>
-                <h3>{{ user.fullName }}</h3>
-                <p class="text-caption mt-1">
-                  {{ user.email }}
-                </p>
-                <v-divider class="my-3"></v-divider>
-                <v-btn depressed rounded text> Edit Account </v-btn>
-                <v-divider class="my-3"></v-divider>
-                <v-btn depressed rounded text> Disconnect </v-btn>
-              </div>
-            </v-list-item-content>
-          </v-card>
-        </v-menu>
-      </v-row>
-    </v-container>
+              <h3>{{ user.fullName }}</h3>
+              <p class="text-caption mt-1">
+                {{ user.email }}
+              </p>
+              <v-divider class="my-3"></v-divider>
+              <v-btn
+                rounded
+                variant="text"
+              >
+                Edit Account
+              </v-btn>
+              <v-divider class="my-3"></v-divider>
+              <v-btn
+                rounded
+                variant="text"
+              >
+                Disconnect
+              </v-btn>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-menu>
+    </v-row>
+  </v-container>
 
     <v-spacer></v-spacer>
     <v-toolbar-title class="ml-3 text-center">EBOOK</v-toolbar-title>
