333f0f36e8e3b0d79f0c9bb4a3a596fce59476b7
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-01-20 10:59:56 +0700

    add profile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,45 @@
 <template>
   <v-app-bar class="app_bar">
-    <v-btn class="header_action" color="white" v-if="!isLogin" @click="goToLogin">
+    <v-btn
+      class="header_action"
+      color="white"
+      v-if="!isLogin"
+      @click="goToLogin"
+    >
       <v-icon class="mr-2">mdi-login</v-icon>
       ล็อกอินเข้าสู่ระบบ / สมัครสมาชิก
     </v-btn>
-    <span class="ml-3" v-if="isLogin">{{ getName() }}</span>
+    <v-container v-if="isLogin">
+      <v-row >
+        <v-menu bottom min-width="200px" rounded offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn icon x-large v-on="on">
+              <v-avatar color="brown" size="48">
+                <span class="white--text text-h5">{{ user.initials }}</span>
+              </v-avatar>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-list-item-content class="justify-center">
+              <div class="mx-auto text-center">
+                <v-avatar color="brown">
+                  <span class="white--text text-h5">{{ user.initials }}</span>
+                </v-avatar>
+                <h3>{{ user.fullName }}</h3>
+                <p class="text-caption mt-1">
+                  {{ user.email }}
+                </p>
+                <v-divider class="my-3"></v-divider>
+                <v-btn depressed rounded text> Edit Account </v-btn>
+                <v-divider class="my-3"></v-divider>
+                <v-btn depressed rounded text> Disconnect </v-btn>
+              </div>
+            </v-list-item-content>
+          </v-card>
+        </v-menu>
+      </v-row>
+    </v-container>
+
     <v-spacer></v-spacer>
     <v-toolbar-title class="ml-3 text-center">EBOOK</v-toolbar-title>
     <v-spacer></v-spacer>
@@ -12,7 +47,7 @@
       <v-icon class="mr-2">mdi-heart</v-icon>
       รายการโปรด
     </v-btn>
-    <v-btn class="header_action" color="white"  @click="goToCart">
+    <v-btn class="header_action" color="white" @click="goToCart">
       <v-icon class="mr-2">mdi-cart</v-icon>
       ตะกร้าสินค้า
     </v-btn>
@@ -25,32 +60,38 @@
 <script>
 import router from "../router";
 export default {
+  data: () => ({
+    user: {
+      initials: "JD",
+      fullName: "John Doe",
+      email: "john.doe@doe.com"
+    },
+  }),
   methods: {
-    logout(){
-      this.$store.dispatch('auth/logout')
+    logout() {
+      this.$store.dispatch("auth/logout");
     },
-    goToLogin(){
-      router.push("/login")
+    goToLogin() {
+      router.push("/login");
     },
-    goToWishlist(){
-      router.push("/wishlist")
+    goToWishlist() {
+      router.push("/wishlist");
     },
-    goToCart(){
-      router.push("/cart")
+    goToCart() {
+      router.push("/cart");
     },
-    getName(){
-      return this.$store.getters['auth/getName']
-    }
+    getName() {
+      return this.$store.getters["auth/getName"];
+    },
   },
   computed: {
-    isLogin(){
-      return this.$store.getters['auth/isLogin']
-    }
-  }
-}
+    isLogin() {
+      return this.$store.getters["auth/isLogin"];
+    },
+  },
+};
 </script>
 <style scoped>
-  
 .header_action {
   border-radius: 10px;
   background-color: rgba(0, 0, 0, 0.12);
