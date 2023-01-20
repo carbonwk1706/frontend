dd277c7684a04171c0e9cdf1dae37c39a5dee9de
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-01-20 11:41:29 +0700

    fix css navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,61 +9,38 @@
       <v-icon class="mr-2">mdi-login</v-icon>
       ล็อกอินเข้าสู่ระบบ / สมัครสมาชิก
     </v-btn>
-    <v-container
-  >
-    <v-row justify="start">
-      <v-menu
-        min-width="200px"
-        rounded
-      >
-        <template v-slot:activator="{ props }">
-          <v-btn
-            icon
-            v-bind="props"
-          >
-            <v-avatar
-              color="brown"
-              size="large"
-            >
-              <span class="text-h5">{{ user.initials }}</span>
-            </v-avatar>
-          </v-btn>
-        </template>
-        <v-card>
-          <v-card-text>
-            <div class="mx-auto text-center">
-              <v-avatar
-                color="brown"
-              >
-                <span class="text-h5">{{ user.initials }}</span>
-              </v-avatar>
-              <h3>{{ user.fullName }}</h3>
-              <p class="text-caption mt-1">
-                {{ user.email }}
-              </p>
-              <v-divider class="my-3"></v-divider>
-              <v-btn
-                rounded
-                variant="text"
-              >
-                Edit Account
-              </v-btn>
-              <v-divider class="my-3"></v-divider>
-              <v-btn
-                rounded
-                variant="text"
-              >
-                Disconnect
-              </v-btn>
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-menu>
-    </v-row>
-  </v-container>
-
+    <div v-if="isLogin" class="ml-5">
+      <v-row>
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-btn class="header_action" v-bind="props">
+              <v-icon class="mr-2">mdi-login</v-icon>
+              สวัสดี {{ getName() }}
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-text>
+              <div class="mx-auto text-center">
+                <v-avatar color="brown">
+                  <span class="text-h5">{{ user.initials }}</span>
+                </v-avatar>
+                <h3>{{ user.fullName }}</h3>
+                <p class="text-caption mt-1">
+                  {{ user.email }}
+                </p>
+                <v-divider class="my-3"></v-divider>
+                <v-btn rounded variant="text"> Edit Account </v-btn>
+                <v-divider class="my-3"></v-divider>
+                <v-btn rounded variant="text"> Disconnect </v-btn>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-menu>
+      </v-row>
+    </div>
     <v-spacer></v-spacer>
-    <v-toolbar-title class="ml-3 text-center">EBOOK</v-toolbar-title>
+    <v-toolbar-title class="align-center text-center">EBOOK</v-toolbar-title>
+
     <v-spacer></v-spacer>
     <v-btn class="header_action" color="white" @click="goToWishlist">
       <v-icon class="mr-2">mdi-heart</v-icon>
@@ -86,7 +63,7 @@ export default {
     user: {
       initials: "JD",
       fullName: "John Doe",
-      email: "john.doe@doe.com"
+      email: "john.doe@doe.com",
     },
   }),
   methods: {
