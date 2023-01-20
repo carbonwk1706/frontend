c2ac3ebbe14a0f6ba94a4af4d1cb2610cb6776cc
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-01-20 10:00:42 +0700

    show modal when user not login and click to cart and wishlist

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,10 +1,34 @@
 <template>
   <div>
     <slot></slot>
+
+    <v-dialog v-model="showModal" max-width="290" persistent>
+      <v-card>
+        <v-card-title class="headline">Error</v-card-title>
+        <v-card-text>กรุณาเข้าสู่ระบบ</v-card-text>
+        <v-card-actions>
+          <v-btn color="green" text @click="goToLoginAndClose"> Close </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+  methods: {
+    goToLogin() {
+      this.$router.push("/");
+    },
+    goToLoginAndClose() {
+      this.goToLogin();
+      this.showModal = false;
+    },
+  },
   computed: {
     isLogin() {
       return this.$store.getters["auth/isLogin"];
@@ -13,13 +37,13 @@ export default {
   watch: {
     isLogin(newValue) {
       if (!newValue) {
-        this.$router.push("/login");
+        this.showModal = true;
       }
     },
   },
   mounted() {
     if (!this.isLogin) {
-      this.$router.push("/login");
+      this.showModal = true;
     }
   },
 };
