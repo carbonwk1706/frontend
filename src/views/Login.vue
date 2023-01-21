3f119e2fc9621ee39f1e315d0fdfe3319c98926c
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-01-22 01:43:38 +0700

    add v-card-title

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,6 +3,10 @@
     <v-sheet>
       <v-card class="mx-auto px-6 py-8" max-width="450">
         <v-form @submit.prevent="login">
+          <v-card-title primary-title class="text-center">
+            ล็อกอินเข้าระบบ
+          </v-card-title>
+          <v-divider></v-divider>
           <v-container>
             <v-text-field
               label="Username"
@@ -27,43 +31,43 @@
               color="success"
               size="large"
               variant="elevated"
-              >เข้าสู่ระบบ</v-btn
+              >ล็อกอินเข้าระบบ</v-btn
             >
           </v-container>
           <v-divider></v-divider>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="success" @click="goToRegister"> 
+            <v-btn color="success" @click="goToRegister">
               สมัครสมาชิก
               <v-icon icon="mdi-chevron-right" end></v-icon>
             </v-btn>
           </v-card-actions>
         </v-form>
-
-        <v-dialog v-model="loading" max-width="350">
-          <v-card>
-            <v-card-title class="center">
-              <div class="img-size">
-                <v-img
-                  src="https://media.tenor.com/M00Zqk6Dx7EAAAAi/peachcat-goma.gif"
-                >
-                </v-img>
-              </div>
-            </v-card-title>
-            <div class="center-loading">
-              <v-progress-circular
-                v-if="loading"
-                :size="50"
-                :width="5"
-                indeterminate
-                color="success"
-              ></v-progress-circular>
-            </div>
-            <v-card-text class="text-center">กำลังเข้าสู่ระบบ</v-card-text>
-          </v-card>
-        </v-dialog>
       </v-card>
     </v-sheet>
+
+    <v-dialog v-model="loading" max-width="350">
+      <v-card>
+        <v-card-title class="center">
+          <div class="img-size">
+            <v-img
+              src="https://media.tenor.com/M00Zqk6Dx7EAAAAi/peachcat-goma.gif"
+            >
+            </v-img>
+          </div>
+        </v-card-title>
+        <div class="center-loading">
+          <v-progress-circular
+            v-if="loading"
+            :size="50"
+            :width="5"
+            indeterminate
+            color="success"
+          ></v-progress-circular>
+        </div>
+        <v-card-text class="text-center">กำลังเข้าสู่ระบบ</v-card-text>
+      </v-card>
+    </v-dialog>
   </AuthLogin>
 </template>
 <script>
@@ -111,13 +115,13 @@ export default {
               this.loading = false;
             }, 2000);
           }
-        } catch(e) {
+        } catch (e) {
           this.alertLogin();
         }
       }
     },
-    goToRegister(){
-      router.push('/register')
+    goToRegister() {
+      router.push("/register");
     },
     alertLogin() {
       this.$swal({
