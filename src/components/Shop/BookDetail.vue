2e20bd49d25802dc7d8e0faf6e46c19e363b56a4
puchong <puchong2543@gmail.com>
2023-01-23 15:08:52 +0700

    Fix size image and add wish button

--- src/components/Shop/BookDetail.vue
+++ src/components/Shop/BookDetail.vue
@@ -3,7 +3,11 @@
     <div class="herdName">Name Book</div>
     <v-row>
       <v-col class="d-flex justify-end">
-        <img src="https://cdn-local.mebmarket.com/meb/server1/225738/Thumbnail/book_detail_large.gif?2" alt="">
+        <v-img
+          src="https://cdn-local.mebmarket.com/meb/server1/225738/Thumbnail/book_detail_large.gif?2"
+          max-height="500"
+          max-width="500"
+        />
       </v-col>
       <v-col>
         <v-col>
@@ -11,7 +15,12 @@
           <v-row>สำนักพิมพ์ publissher</v-row>
           <v-row>หมวดหมู่ category</v-row>
           <v-row>
-            <v-btn> ฿ 1000 </v-btn>
+            <v-btn color="success" rounded> ฿ 1000 </v-btn>
+          </v-row>
+          <v-row>
+            <v-btn icon color="pink" @click="disabled">
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
           </v-row>
         </v-col>
       </v-col>
@@ -25,11 +34,11 @@ export default {
 };
 </script>
 
-<style>
-.herdName{
+<style scoped>
+.herdName {
   display: flex;
   justify-content: center;
-  padding: 5px;
+  margin-bottom: 30px;
   font-size: larger;
   font-weight: bold;
 }
