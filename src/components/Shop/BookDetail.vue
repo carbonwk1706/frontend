c029a0a8d74ea12faebf5e2a5fa813ba3f3e5ef7
puchong <puchong2543@gmail.com>
2023-01-24 14:46:47 +0700

    Format code

--- src/components/Shop/BookDetail.vue
+++ src/components/Shop/BookDetail.vue
@@ -3,11 +3,7 @@
     <div class="herdName">{{ book.name }}</div>
     <v-row>
       <v-col class="d-flex justify-end">
-        <v-img
-          :src="book.imageBook"
-          max-height="500"
-          max-width="500"
-        />
+        <v-img :src="book.imageBook" max-height="500" max-width="500" />
       </v-col>
       <v-col class="description">
         <v-col>
@@ -18,13 +14,17 @@
           <v-row>หมวดหมู่ {{ book.category }}</v-row>
           <br />
           <v-row>
-            <v-btn color="success" rounded width="100"> ฿ {{ book.price }} </v-btn>
+            <v-btn color="success" rounded width="100">
+              ฿ {{ book.price }}
+            </v-btn>
           </v-row>
           <br />
           <v-row>
-            <v-btn icon color="pink" size="50">
+
+            <v-btn icon color="white" @click="wishProcess()">
               <v-icon>mdi-heart</v-icon>
             </v-btn>
+
           </v-row>
         </v-col>
       </v-col>
@@ -33,7 +33,7 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "BookDetail",
@@ -45,17 +45,23 @@ export default {
         publisher: "",
         catagory: "",
         price: "",
-        imgaeBook: null
-
-      }
-    }
+        imgaeBook: null,
+      },
+    };
+  },
+  methods: {
+    wishProcess() {
+      
+    },
   },
   mounted() {
-    axios.get("http://localhost:3000/books/"+this.$route.params.id).then((result) => {
-      console.log(JSON.parse(JSON.stringify(result)))
-      this.book = result.data;
-    });
-  }
+    axios
+      .get("http://localhost:3000/books/" + this.$route.params.id)
+      .then((result) => {
+        console.log(JSON.parse(JSON.stringify(result)));
+        this.book = result.data;
+      });
+  },
 };
 </script>
 
