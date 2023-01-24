714248b21d7cbe79cca145dd4297f461c5c404e6
puchong <puchong2543@gmail.com>
2023-01-24 11:02:08 +0700

    Connect backend

--- src/components/Shop/ShopTable.vue
+++ src/components/Shop/ShopTable.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="mb-5">
-    <h1>Header</h1>
-    <hr />
-  </div>
   <v-container class="grey lighten-5">
+    <div class="mb-5 d-flex justify-center">
+      <h1>ร้านค้า</h1>
+      
+    </div>
+    <hr class="mb-6" />
     <v-row no-gutters>
       <v-col v-for="(item, index) in books" :key="index">
-        <v-card class="mx-auto" max-width="180" @click="showDetail">
-          <v-img :src="item.image" height="250px"></v-img>
+        <v-card class="mx-auto" max-width="180" @click="showDetail(item)">
+          <v-img :src="item.imageBook" height="250px"></v-img>
           <v-card-title> {{ item.name }} </v-card-title>
-
           <v-card-subtitle>
             {{ item.author }}/{{ item.publisher }}
           </v-card-subtitle>
           <v-row class="d-flex justify-end ma-3">
-            <v-btn color="success"> ฿ {{ item.price }} </v-btn>
+            <v-btn color="success" @click="buyItem(item)">
+              ฿ {{ item.price }}
+            </v-btn>
           </v-row>
         </v-card>
       </v-col>
@@ -23,69 +25,30 @@
 </template>
 
 <script>
-import router from '@/router';
+import axios from "axios";
+
 export default {
   name: "ShopTable",
   data() {
     return {
-      books: [
-        {
-          id: "1",
-          name: "book1",
-          author: "name1",
-          publisher: "name1",
-          category: "type1",
-          price: 100,
-          image:
-            "https://cdn-local.mebmarket.com/meb/server1/225386/Thumbnail/web_large2.gif?2",
-        },
-        {
-          id: "2",
-          name: "book2",
-          author: "name1",
-          publisher: "name1",
-          category: "type1",
-          price: 100,
-          image:
-            "https://cdn-local.mebmarket.com/meb/server1/225386/Thumbnail/web_large2.gif?2",
-        },
-        {
-          id: "3",
-          name: "book3",
-          author: "name1",
-          publisher: "name1",
-          category: "type1",
-          price: 100,
-          image:
-            "https://cdn-local.mebmarket.com/meb/server1/225386/Thumbnail/web_large2.gif?2",
-        },
-        {
-          id: "4",
-          name: "book4",
-          author: "name1",
-          publisher: "name1",
-          category: "type1",
-          price: 100,
-          image:
-            "https://cdn-local.mebmarket.com/meb/server1/225386/Thumbnail/web_large2.gif?2",
-        },
-        {
-          id: "5",
-          name: "book5",
-          author: "name1",
-          publisher: "name1",
-          category: "type1",
-          price: 100,
-          image:
-            "https://cdn-local.mebmarket.com/meb/server1/225386/Thumbnail/web_large2.gif?2",
-        },
-      ],
+      books: [],
     };
   },
   methods: {
-    showDetail(){
-      router.push("/book")
-    }
+    showDetail(item) {
+      console.log(item.id);
+      this.$router.push(`/book/${item.id}`);
+    },
+    buyItem(item) {
+      console.log(item);
+      alert("BUY");
+    },
+  },
+  mounted() {
+    axios.get("http://localhost:3000/books/").then((result) => {
+      console.log(result);
+      this.books = result.data;
+    });
   },
 };
 </script>
