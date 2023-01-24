bf14f70cfdb57cf46a846fefa7ae7edf7bf23cf4
puchong <puchong2543@gmail.com>
2023-01-24 11:01:06 +0700

    Add CartList.vue

---
+++ src/components/Shop/CartList.vue
@@ -0,0 +1,24 @@
+<template>
+<v-container>
+    <div class="herdName">ตะกร้า</div>
+    <v-data-table></v-data-table>
+    
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.herdName {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+  font-size: 36px;
+  font-weight: bold;
+  
+}
+</style>
