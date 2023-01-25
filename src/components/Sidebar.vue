6c6ba5cbe12721f83f9dadab4d130d650c6abd9e
carbon wk <wkcarbon1706@gmail.com>
2023-01-25 22:11:34 +0700

    Fix ui Sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,53 +1,68 @@
 <template>
-  <v-app-bar color="green" prominent></v-app-bar>
-  <v-navigation-drawer class="deep-purple accent-4" dark permanent>
-    <v-list>
+  <v-app-bar :elevation="10" rounded color="#00af70"></v-app-bar>
+  <v-navigation-drawer
+    v-model="drawer"
+    :rail="rail"
+    permanent
+    @click="rail = false"
+  >
+    <v-list-item nav>
+      <template v-slot:append>
+        <v-btn
+          variant="text"
+          :icon="rail ? 'mdi-chevron-right' : 'mdi-chevron-left '"
+          @click.stop="toggleRail"
+        ></v-btn>
+      </template>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
       <v-list-item
-        v-for="item in items"
-        :key="item.title"
-        @click="goTo(item.path)"
-      >
-      <v-list-item-group></v-list-item-group>
-        <v-list-item-icon>
-          <v-icon >{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+        prepend-icon="mdi-account"
+        title="AdminTable"
+        value="account"
+        @click="goToAdminTable()"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-account-group-outline"
+        title="UserTable"
+        value="users"
+        @click="goToUserTable()"
+      ></v-list-item>
     </v-list>
-
-    <template v-slot:append>
-      <div class="pa-2">
-        <v-btn block> Logout </v-btn>
-      </div>
-    </template>
   </v-navigation-drawer>
 </template>
-
 <script>
 import router from "../router";
 export default {
   drawer: false,
   data() {
     return {
+      drawer: true,
       items: [
-        { title: "Dashboard", icon: "mdi-view-dashboard", path: "/usertable" },
-        { title: "Account", icon: "mdi-account-box" },
-        { title: "Admin", icon: "mdi-gavel" },
+        { title: "Home", icon: "mdi-home-city", path: "/admin" },
+        { title: "Admins", icon: "mdi-account", path: "/usertable" },
+        {
+          title: "Users",
+          icon: "mdi-account-group-outline",
+          path: "/usertable",
+        },
       ],
+      rail: true,
     };
   },
   methods: {
-    goTo(path) {
-      this.drawer = false;
-      router.push(path);
+    toggleRail() {
+      this.rail = !this.rail;
+    },
+    goToUserTable() {
+      router.push("/usertable");
+    },
+    goToAdminTable() {
+      router.push("/admintable");
     },
   },
 };
 </script>
-<style >
-
-
-</style>
