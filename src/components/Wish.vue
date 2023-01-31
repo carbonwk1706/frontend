3431567399c8367967de2d798fa1622278b35a7e
puchong <puchong2543@gmail.com>
2023-01-31 14:07:45 +0700

    Add wish UI

---
+++ src/components/Wish.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container class="grey lighten-5">
+    <div class="mb-5 d-flex justify-start">
+      <h1>รายการที่อยากได้</h1>
+    </div>
+    <hr class="mb-6" />
+    <v-row>
+      <v-col v-for="(item, index) in wishList" :key="index">
+        <v-card class="mx-auto" max-width="180" @click="showDetail(item)">
+          <v-img :src="item.imageBook" height="250px"></v-img>
+          <v-card-title> {{ item.name }} </v-card-title>
+          <v-card-subtitle>
+            {{ item.author }}/{{ item.publisher }}
+          </v-card-subtitle>
+          <v-row class="d-flex justify-end ma-3">
+            <v-btn color="success" @click="addItem(item)">
+              ฿ {{ item.price }}
+            </v-btn>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import api from "@/services/api";
+
+export default {
+  name: "Wish",
+  data() {
+    return {
+      wishList: [],
+    };
+  },
+  mounted() {
+    api.get("/books/").then((result) => {
+      console.log(result);
+      this.wishList = result.data;
+    });
+  },
+};
+</script>
+
+<style></style>
--- src/views/Wishlist.vue
+++ src/views/Wishlist.vue
@@ -1,14 +1,16 @@
 <template>
   <AuthWishlist>
-    <h1>Wishlist</h1>
+    <Wish />
   </AuthWishlist>
 </template>
 <script>
 import AuthWishlist from "../components/AuthWishlist.vue";
+import Wish from "@/components/Wish.vue";
 
 export default {
   components: {
     AuthWishlist,
+    Wish
   },
 };
 </script>
