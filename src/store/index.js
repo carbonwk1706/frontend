d97f4102f04c0cd145e916f5acda4b9b84a27621
puchong <puchong2543@gmail.com>
2023-01-31 13:58:06 +0700

    Add check duplication in cart

--- src/store/index.js
+++ src/store/index.js
@@ -1,43 +1,50 @@
-import { createStore } from 'vuex'
-import auth from './modules/auth'
-import authAdmin from './modules/authAdmin'
+import { createStore } from "vuex";
+import auth from "./modules/auth";
+import authAdmin from "./modules/authAdmin";
 export default createStore({
   state: {
-    cartList: []
+    cartList: [],
   },
   getters: {
-    cartListCount(state){
+    cartListCount(state) {
       return state.cartList.length;
     },
-    getTotalPrice(state){
-      var total = 0
+    getTotalPrice(state) {
+      var total = 0;
 
-      state.cartList.forEach(item => {
-        total += parseInt(item.price)
-      })
-      
+      state.cartList.forEach((item) => {
+        total += parseInt(item.price);
+      });
+
       return total;
-    }
+    },
   },
   mutations: {
-     addItem(state, item){
-      state.cartList.push(item)
-     },
-     delItem(state, product){
-      var index = state.cartList.findIndex(productIndex => productIndex._id == product)
-      state.cartList.splice(index, 1)
-    }
+    addItem(state, product) {
+      let duplicaProduct = state.cartList.find((item) => {
+        return item._id === product._id;
+      });
+      if (!duplicaProduct) {
+        state.cartList.push(product);
+      }
+    },
+    delItem(state, product) {
+      var index = state.cartList.findIndex(
+        (productIndex) => productIndex._id == product
+      );
+      state.cartList.splice(index, 1);
+    },
   },
   actions: {
-    addItemToCart(context, item){
-      context.commit('addItem', item)
+    addItemToCart(context, item) {
+      context.commit("addItem", item);
     },
-    delItemCart(context, product){
-      context.commit('delItem', product)
-    }
+    delItemCart(context, product) {
+      context.commit("delItem", product);
+    },
   },
   modules: {
     auth,
-    authAdmin
-  }
-})
+    authAdmin,
+  },
+});
