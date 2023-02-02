a55eb5cbda3b4dbecfa4c35920c78898522ccd8d
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-02-02 09:15:25 +0700

    fix warning

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -210,7 +210,7 @@
   <ManageUserForm
     :editModal="editModal"
     @update:isVisible="editModal = $event"
-    @someEvent="callback"
+    @update:someEvent="callback"
   />
 </template>
 <script>
