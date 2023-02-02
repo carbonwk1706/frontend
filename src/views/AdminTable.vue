c8ab7fb6449cfb12f25bb5781e140c0c78a41d7c
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-02-02 11:41:31 +0700

    test get all request and detail request and can approve and reject requset

--- src/views/AdminTable.vue
+++ src/views/AdminTable.vue
@@ -1,13 +1,82 @@
 <template>
-    <div>
-        TEST2
-    </div>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">ลำดับ</th>
+        <th class="text-left">คำร้อง</th>
+        <th class="text-left">สถานะ</th>
+        <th class="text-left"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(index, item) in requestStatus" :key="index">
+        <td>{{ item + 1 }}</td>
+        <td>{{ requestStatus[item].request }}</td>
+        <td>{{ requestStatus[item].status }}</td>
+        <td>
+          <v-btn
+            variant="flat"
+            color="grey"
+            class="mr-3"
+            @click="showDetail(requestStatus[item]._id)"
+          >
+            ดูรายละเอียด
+          </v-btn>
+          <v-btn
+            variant="flat"
+            color="success"
+            class="mr-3"
+            @click="approveRequest(requestStatus[item]._id)"
+          >
+            อนุมัติ
+          </v-btn>
+          <v-btn variant="flat" color="error" @click="rejectsRequest(requestStatus[item]._id)"> ยกเลิก </v-btn>
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
 <script>
+import api from "@/services/api";
+
 export default {
-
-}
+  data() {
+    return {
+      requestStatus: [],
+    };
+  },
+  methods: {
+    showDetail(id) {
+      this.$router.push(`/request/${id}`);
+    },
+    async approveRequest(id) {
+      try {
+        await api.patch(`/request/${id}/approve`);
+        this.fetchApi()
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async rejectsRequest(id) {
+      try {
+        await api.patch(`/request/${id}/reject`);
+        this.fetchApi()
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async fetchApi() {
+      try {
+        const res = await api.get("/request");
+        this.requestStatus = res.data.requests;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  mounted() {
+    this.fetchApi();
+  },
+};
 </script>
-<style>
-
-</style>
+<style></style>
