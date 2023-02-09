4e54d0b28fb333b4e70f44bcdc6ce3c82b051c17
carbon wk <wkcarbon1706@gmail.com>
2023-02-09 13:10:11 +0700

    add EditAdminForm

--- src/views/AdminTable.vue
+++ src/views/AdminTable.vue
@@ -10,15 +10,27 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(item, index) in userItems" :key="index" >
+      <tr v-for="(item, index) in userItems" :key="index">
         <td>{{ item.name }}</td>
         <td>{{ item.username }}</td>
         <td>{{ item.email }}</td>
         <td>{{ item.gender }}</td>
         <td>{{ item.roles }}</td>
-        <td >
-         <v-btn variant="flat" color="success" class="mr-3" @click="editUser(item)">แก้ไข</v-btn>
-          <v-btn variant="flat" color="error" class="mr-3" @click="deleteUser(item)">ลบ</v-btn>
+        <td>
+          <v-btn
+            variant="flat"
+            color="success"
+            class="mr-3"
+            @click="editUser(item)"
+            >แก้ไข</v-btn
+          >
+          <v-btn
+            variant="flat"
+            color="error"
+            class="mr-3"
+            @click="deleteUser(item)"
+            >ลบ</v-btn
+          >
         </td>
       </tr>
     </tbody>
@@ -26,56 +38,53 @@
 </template>
 
 <script>
-
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  components: {
-
-  },
+  components: {},
   data() {
     return {
       userItems: [],
       selectedUser: {},
-      showForm: false
-
+      showForm: false,
     };
   },
   methods: {
-      deleteUser(user){
-           axios.delete('http://localhost:3000/users/' + user._id, user)
-          .then(
-            (result)=>{
-              this.userItems = result.data;
-              this.fetchApi()
-            }
-          )
-      },
-      editUser(user) {
-        this.$router.push(`/usertable/${user._id}`);
-      },
-      updateUser(updatedUser) {
-        axios.put('http://localhost:3000/users/' + updatedUser._id, updatedUser)
-        .then(
-          (result)=>{
-            this.userItems = result.data;
-            this.fetchApi()
-          }
-        )
-        this.showForm = false
-      },
-      fetchApi(){
-
-        axios.get('http://localhost:3000/users').then(
-        (result)=>{
+    deleteUser(user) {
+      axios
+        .delete("http://localhost:3000/users/" + user._id, user)
+        .then((result) => {
           this.userItems = result.data;
-          console.log(this.userItems)
-        }
-      )
-      }
+          this.fetchApi();
+        });
+    },
+    editUser(user) {
+      this.$router.push(`/usertable/${user._id}`);
+    },
+    updateUser(updatedUser) {
+      axios
+        .put("http://localhost:3000/users/" + updatedUser._id, updatedUser)
+        .then((result) => {
+          this.userItems = result.data;
+          this.fetchApi();
+        });
+      this.showForm = false;
+    },
+    fetchApi() {
+      axios.get("http://localhost:3000/users").then((result) => {
+        this.userItems = [];
+        result.data.forEach((user) => {
+          if (["ADMIN", "LOCAL_ADMIN"].includes(user.roles)) {
+            this.userItems.push(user);
+          }
+        });
+        console.log("TestResult",result.data);
+        console.log("TestUserItem",this.userItems);
+      });
+    },
   },
   mounted() {
-      this.fetchApi()
+    this.fetchApi();
   },
 };
 </script>
