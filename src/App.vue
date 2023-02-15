4bab89b0946c599881b8e09e2417197676d040db
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-02-15 19:46:09 +0700

    just fix return in app js

--- src/App.vue
+++ src/App.vue
@@ -129,7 +129,6 @@ export default {
           "/register",
           "/login",
           "/admin",
-          "/usertable/",
           "/usertable",
           "/admintable",
           "/approvetable",
