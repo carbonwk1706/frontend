6589626dc19d8e311463a56e5d5db063ef59391d
carbon wk <wkcarbon1706@gmail.com>
2023-02-15 14:10:01 +0700

    check duplicate

--- src/views/EditUserForm.vue
+++ src/views/EditUserForm.vue
@@ -29,7 +29,7 @@
             />
           </v-col>
         </v-row>
-        <v-row>
+        <!-- <v-row>
           <v-col>
             <v-text-field
               label="Password"
@@ -37,7 +37,7 @@
               :rules="[(v) => !!v || 'Password is required']"
             />
           </v-col>
-        </v-row>
+        </v-row> -->
         <v-row>
           <v-col>
             <v-select
@@ -64,6 +64,7 @@
   <v-dialog
     v-model="showConfirmDialog"
     persistent
+    style="z-index: 900"
     :center="true"
     max-width="500"
     :padding="20"
@@ -74,7 +75,7 @@
         >ต้องการยืนยันการแก้ไขหรือไม่</v-card-text
       >
       <v-card-actions class="text-center">
-        <v-btn color="success" @click="submit">OK</v-btn>
+        <v-btn color="success" @click="changeDuplicate">OK</v-btn>
         <v-btn color="Grey" text @click="showConfirmDialog = false"
           >Cancel</v-btn
         >
@@ -91,8 +92,10 @@ export default {
     return {
       showConfirmDialog: false,
       valid: false,
+      form: { username: "", email: "", },
       lazy: false,
       userItems: [],
+
       usernameRule: [
         (v) => !!v || "กรุณากรอก username",
         (v) => !/[ ]/.test(v) || "ห้ามเว้นวรรค",
@@ -122,27 +125,123 @@ export default {
     };
   },
   methods: {
+    async changeDuplicate() {
+      const { valid } = await this.$refs.form.validate();
+      if (!valid) {
+        this.showAlert("กรุณากรอกข้อมูลให้ถูกต้อง");
+      } else {
+        try {
+          const res = await api.post(
+            "/checkDuplicateUser/" + this.$route.params.id,
+            {
+              username: this.userItems.username,
+              email: this.userItems.email,
+            }
+          );
+          if (
+            res.status === 200 &&
+            res.data.message === "Username and Email already exists"
+          ) {
+
+            this.showAlert("Username and Email already exists");
+          } else if (
+            res.status === 200 &&
+            res.data.message === "Username already exists"
+          ) {
+            this.showAlert("Username already exists");
+          } else if (
+            res.status === 200 &&
+            res.data.message === "Email already exists"
+          ) {
+            this.showAlert("Email already exists");
+          } else if (
+            res.status === 201 &&
+            res.data.message === "Username and Email already"
+          ) {
+            this.showAlertSucsess("แก้ไขสำเร็จ");
+            this.submit();
+          }
+        } catch (error) {
+          console.log("ERROR");
+        }
+      }
+    },
     changeStatus() {
       this.status = !this.status;
       alert(this.status);
     },
     alertvalidate() {},
     submit() {
-      this.$refs.form.validate().then((valid) => {
-        if (valid) {
-          api
-            .put("/users/" + this.$route.params.id, this.userItems)
-            .then(() => {
-              this.$router.push("/usertable");
-            });
-        }
+      api.put("/users/" + this.$route.params.id, this.userItems).then(() => {
+        this.$router.push("/usertable");
+      });
+    },
+    showAlert(text) {
+      this.showConfirmDialog = false;
+      this.$swal({
+        confirmButtonColor: "#00af70",
+        allowOutsideClick: false,
+        width: "500",
+        text: text,
+        icon: "error",
+        button: "OK",
+      });
+    },
+    showAlertSucsess(text) {
+      this.showConfirmDialog = false;
+      this.$swal({
+        confirmButtonColor: "#00af70",
+        allowOutsideClick: false,
+        width: "500",
+        icon: "success",
+        text: text,
+        button: "OK",
+      });
+    },
+    resetForm(){
+      this.formEdit.username = "",
+      this.formEdit.email= ""
+
+    },
+    // alertUsername() {
+    //   this.showConfirmDialog =false ;
+    //   this.$swal({
+    //     confirmButtonColor: "#00af70",
+    //     allowOutsideClick: false,
+    //     width: "500",
+    //     text: "Username and Email already exists ",
+    //     icon: "error",
+    //     button: "OK",
+    //   }
+    //   );
+    // },
+    // alertEmail() {
+    //   this.showConfirmDialog =false ;
+    //   this.$swal({
+    //     confirmButtonColor: "#00af70",
+    //     allowOutsideClick: false,
+    //     width: "500",
+    //     text: "Username and Email already exists ",
+    //     icon: "error",
+    //     button: "OK",
+    //   }
+    //   );
+    // },
+    hideDialog() {
+      this.resetForm();
+      this.$emit("update:showConfirmDialog", false);
+    },
+    resetFormApi() {
+      this.fetchApi();
+    },
+    fetchApi() {
+      api.get("/users/" + this.$route.params.id).then((result) => {
+        this.userItems = result.data;
       });
     },
   },
   mounted() {
-    api.get("/users/" + this.$route.params.id).then((result) => {
-      this.userItems = result.data;
-    });
+    this.fetchApi();
   },
 };
 </script>
