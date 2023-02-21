8101efed7a9ed92f0ff6adb21e5967c37979556d
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-02-21 19:50:56 +0700

    fix size footer

--- src/components/Coin/Footer.vue
+++ src/components/Coin/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer class="footer-bg d-flex flex-row" max-width="auto">
+  <v-footer class="footer-bg d-flex flex-row"  :style="{ height: '50px', position: 'fixed', bottom: 0 }">
     <v-container class="container-max-width pa-0">
       <div class="center">Copyright 2023 Ebook coin . All rights reserved.</div>
     </v-container>
@@ -8,13 +8,15 @@
 <script>
 
 export default {
-  name: 'FooterVoin'
+  name: 'FooterCoin'
 };
 </script>
 <style scoped>
 .footer-bg {
   background-color: #ccc;
   color: #fff;
+  width: 100%;
+  max-width: auto;
 }
 .container-max-width {
   max-width: 1140px;
@@ -25,3 +27,4 @@ export default {
   align-items: center;
 }
 </style>
+
