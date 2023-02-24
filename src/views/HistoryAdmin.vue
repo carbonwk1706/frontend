76830d460ea7ae8791f6413a7924bb0c93fe10a2
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-02-24 14:40:35 +0700

    add auth admin and can see detail request

--- src/views/HistoryAdmin.vue
+++ src/views/HistoryAdmin.vue
@@ -1,86 +1,120 @@
 <template>
-  <v-row>
-    <v-col cols="12">
-      <h2>ประวัติการทำรายการของฉัน</h2>
-    </v-col>
-  </v-row>
-  <v-row class="mb-1">
-    <v-col cols="6" class="text-start">
-      <div class="select-width">
-        <v-select
-          density="compact"
-          v-model="select"
-          :items="selectItem"
-          variant="outlined"
-        ></v-select>
-      </div>
-    </v-col>
-    <v-col  cols="6" class="pa-0 d-flex justify-end">
-      <v-pagination
-        class="text-pagination"
-        v-model="page"
-        :length="pages"
-        circle
-      ></v-pagination>
-    </v-col>
-  </v-row>
-  <v-table class="mt-5">
-    <thead>
-      <tr>
-        <th class="text-left">ลำดับ</th>
-        <th class="text-left">วันที่ทำรายการ</th>
-        <th class="text-left">รายการที่ทำ</th>
-        <th class="text-left">สถานะ</th>
-        <th class="text-left"></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(item, index) in request" :key="index">
-        <td>{{ index + 1 }}</td>
-        <td>{{ formatTime(item.approvedAt) }}</td>
-        <td>{{ item.request }}</td>
-        <td>{{ item.status }}</td>
-        <td>
-          <v-btn
-            variant="flat"
-            color="grey"
-            class="mr-3"
-            @click="showDetail(item._id)"
-          >
-            รายละเอียด
-          </v-btn>
-        </td>
-      </tr>
-    </tbody>
-  </v-table>
+  <AuthAdmin v-if="isLogin"
+    ><v-row>
+      <v-col cols="12">
+        <h2>ประวัติการทำรายการของฉัน</h2>
+      </v-col>
+    </v-row>
+    <v-row class="mb-1">
+      <v-col cols="6" class="text-start">
+        <div class="select-width">
+          <v-select
+            density="compact"
+            v-model="select"
+            :items="selectItem"
+            variant="outlined"
+          ></v-select>
+        </div>
+      </v-col>
+      <v-col v-if="request.length > 0" cols="6" class="pa-0 d-flex justify-end">
+        <v-pagination
+          class="text-pagination"
+          v-model="page"
+          :length="pages"
+          circle
+        ></v-pagination>
+      </v-col>
+    </v-row>
+    <v-table v-if="request.length > 0" class="mt-5">
+      <thead>
+        <tr>
+          <th class="text-left">ลำดับ</th>
+          <th class="text-left">วันที่ทำรายการ</th>
+          <th class="text-left">รายการที่ทำ</th>
+          <th class="text-left">สถานะ</th>
+          <th class="text-left"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in request" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ formatTime(item.approvedAt) }}</td>
+          <td>{{ item.request }}</td>
+          <td>{{ item.status }}</td>
+          <td>
+            <v-btn
+              variant="flat"
+              color="grey"
+              class="mr-3"
+              @click="showDetail(item)"
+            >
+              ดูรายละเอียด
+            </v-btn>
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+    <v-row v-else>
+      <v-divider class="mb-6"></v-divider>
+      <v-col cols="12">
+        <div class="d-flex justify-center">
+          <img
+            src="https://www.mebmarket.com/web/dist/assets/images/imgMebcatMebphone@2x.png"
+            width="200"
+            height="200"
+          />
+        </div>
+        <div class="text-noRequest text-center">
+          <p>ขออภัยด้วยนะครับ</p>
+        </div>
+        <div class="text-center mt-3">
+          <p class="text-muted">ไม่พบข้อมูลในหัวข้อที่คุณกำลังชมค่ะ</p>
+        </div>
+      </v-col>
+    </v-row>
 
-  
-  <v-row  class="mt-12">
-    <v-col cols="12" class="pa-0 d-flex justify-center">
-      <v-pagination
-        class="text-pagination"
-        v-model="page"
-        :length="pages"
-        circle
-      ></v-pagination>
-    </v-col>
-  </v-row>
+    <v-row v-if="request.length > 0" class="mt-12">
+      <v-col cols="12" class="pa-0 d-flex justify-center">
+        <v-pagination
+          class="text-pagination"
+          v-model="page"
+          :length="pages"
+          circle
+        ></v-pagination>
+      </v-col>
+    </v-row>
+
+  </AuthAdmin>
+  <AuthAdmin v-else></AuthAdmin>
 </template>
 <script>
 import api from "@/services/api";
+import AuthAdmin from "../components/AuthAdmin.vue";
 import moment from "moment";
 
 export default {
+  components: {
+    AuthAdmin,
+  },
   data() {
     return {
       request: [],
       select: "รายการทั้งหมด",
-      selectItem: ["รายการทั้งหมด", "รายการขอขายอีบุ๊ค", "รายการขอเติมCoin"],
+      selectItem: ["รายการทั้งหมด", "รายการขอขายอีบุ๊ค", "รายการขอเติม Coin"],
       page: 1,
       itemsPerPage: 40,
     };
   },
   methods: {
+    showDetail(item) 
+    {
+      console.log(item.request)
+      if(item.request === "คำร้องขอสมัครขายอีบุ๊ค"){
+        this.$router.push(`/request/${item._id}`);
+      }else if(item.request === "คำร้องขอเพิ่ม Coin"){
+        this.$router.push(`/requestcoin/${item._id}`);
+      }
+    },
     getId() {
       return this.$store.getters["authAdmin/getId"];
     },
@@ -107,6 +141,9 @@ export default {
     pages() {
       return Math.ceil(this.request.length / this.itemsPerPage);
     },
+    isLogin() {
+      return this.$store.getters["authAdmin/isLogin"];
+    },
   },
   watch: {
     select(newValue) {
@@ -115,19 +152,25 @@ export default {
           this.fetchApi();
         } else if (newValue === "รายการขอขายอีบุ๊ค") {
           this.getHistoryRequest();
-        } else if (newValue === "รายการขอเติมCoin") {
+        } else if (newValue === "รายการขอเติม Coin") {
           this.getHistoryReceipts();
         }
       }
     },
   },
   mounted() {
-    this.fetchApi();
+    if (this.isLogin) {
+      this.fetchApi();
+    }
   },
 };
 </script>
 <style scoped>
 .select-width {
   width: 200px;
+}
+.text-noRequest {
+  font-size: 18px;
+  font-weight: bold;
 }
 </style>
