156c7eb7a0d257cc3c0c90caab0b35bf3e180097
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-02-24 14:10:07 +0700

    add filter and pagination in historyAdmin

--- src/views/HistoryAdmin.vue
+++ src/views/HistoryAdmin.vue
@@ -1,5 +1,30 @@
 <template>
-  <v-table>
+  <v-row>
+    <v-col cols="12">
+      <h2>ประวัติการทำรายการของฉัน</h2>
+    </v-col>
+  </v-row>
+  <v-row class="mb-1">
+    <v-col cols="6" class="text-start">
+      <div class="select-width">
+        <v-select
+          density="compact"
+          v-model="select"
+          :items="selectItem"
+          variant="outlined"
+        ></v-select>
+      </div>
+    </v-col>
+    <v-col  cols="6" class="pa-0 d-flex justify-end">
+      <v-pagination
+        class="text-pagination"
+        v-model="page"
+        :length="pages"
+        circle
+      ></v-pagination>
+    </v-col>
+  </v-row>
+  <v-table class="mt-5">
     <thead>
       <tr>
         <th class="text-left">ลำดับ</th>
@@ -11,7 +36,7 @@
     </thead>
     <tbody>
       <tr v-for="(item, index) in request" :key="index">
-        <td>{{ index+1 }}</td>
+        <td>{{ index + 1 }}</td>
         <td>{{ formatTime(item.approvedAt) }}</td>
         <td>{{ item.request }}</td>
         <td>{{ item.status }}</td>
@@ -28,6 +53,18 @@
       </tr>
     </tbody>
   </v-table>
+
+  
+  <v-row  class="mt-12">
+    <v-col cols="12" class="pa-0 d-flex justify-center">
+      <v-pagination
+        class="text-pagination"
+        v-model="page"
+        :length="pages"
+        circle
+      ></v-pagination>
+    </v-col>
+  </v-row>
 </template>
 <script>
 import api from "@/services/api";
@@ -37,6 +74,10 @@ export default {
   data() {
     return {
       request: [],
+      select: "รายการทั้งหมด",
+      selectItem: ["รายการทั้งหมด", "รายการขอขายอีบุ๊ค", "รายการขอเติมCoin"],
+      page: 1,
+      itemsPerPage: 40,
     };
   },
   methods: {
@@ -46,27 +87,47 @@ export default {
     formatTime(item) {
       return moment(item).format("MM/DD/YYYY, h:mm:ss a");
     },
-    getHistoryRequest(){
+    getHistoryRequest() {
       api.get("/history/request/" + this.getId()).then((result) => {
-      this.request = result.data.processedRequests;
-    });
+        this.request = result.data.processedRequests;
+      });
     },
-    getHistoryReceipts(){
+    getHistoryReceipts() {
       api.get("/history/receipts/" + this.getId()).then((result) => {
-      this.receipts = result.data.processedReceipts;
-    });
+        this.request = result.data.processedReceipts;
+      });
     },
-    fetchApi(){
+    fetchApi() {
       api.get("/history/all/" + this.getId()).then((result) => {
-      console.log(result)
-    });
-    }
+        this.request = result.data.combinedData;
+      });
+    },
+  },
+  computed: {
+    pages() {
+      return Math.ceil(this.request.length / this.itemsPerPage);
+    },
+  },
+  watch: {
+    select(newValue) {
+      if (newValue) {
+        if (newValue === "รายการทั้งหมด") {
+          this.fetchApi();
+        } else if (newValue === "รายการขอขายอีบุ๊ค") {
+          this.getHistoryRequest();
+        } else if (newValue === "รายการขอเติมCoin") {
+          this.getHistoryReceipts();
+        }
+      }
+    },
   },
   mounted() {
-    this.getHistoryRequest()
-    this.getHistoryReceipts()
-    this.fetchApi()
+    this.fetchApi();
   },
 };
 </script>
-<style></style>
+<style scoped>
+.select-width {
+  width: 200px;
+}
+</style>
