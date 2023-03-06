c0be083621a79ded986f74551f759fae785d8686
carbon wk <wkcarbon1706@gmail.com>
2023-03-07 00:46:16 +0700

    add BookQueue

---
+++ src/views/UserSell/BookUserSell/BookQueue.vue
@@ -0,0 +1,27 @@
+<template>
+  <AuthSell v-if="isLogin">
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12">
+          <h2>รายการหนังสือ</h2>
+        </v-col>
+      </v-row>
+    </v-container>
+  </AuthSell>
+  <AuthSell v-if="!isLogin"> </AuthSell>
+</template>
+<script>
+import AuthSell from "@/components/AuthSell.vue";
+export default {
+  components: {
+    AuthSell,
+  },
+
+  computed: {
+    isLogin() {
+      return this.$store.getters["auth/isLogin"];
+    },
+  },
+};
+</script>
+<style></style>
