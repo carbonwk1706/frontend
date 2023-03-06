a875bd9dffd34feb0a4aa9d6866c73ca5399de37
puchong <puchong2543@gmail.com>
2023-03-07 00:33:50 +0700

    Just css all review and check review length in mounted

--- src/components/Shop/AllReview.vue
+++ src/components/Shop/AllReview.vue
@@ -4,75 +4,159 @@
       <v-col cols="6" class="d-flex justify-start text-start">
         <h2 class="display-1 font-weight-bold">รีวิวทั้งหมด</h2>
       </v-col>
-      <v-col cols="6" class="d-flex justify-end pa-0">
-        <v-pagination
-          class="text-pagination"
-          v-model="page"
-          :length="pages"
-          circle
-        ></v-pagination>
-      </v-col>
     </v-row>
     <v-divider class="mb-6"></v-divider>
-    <v-row class="mb-1">
-      <v-col
-        cols="12"
-        v-for="(book, index) in reviews"
-        :key="index"
-        class="mb-5 d-flex justify-center"
-      >
-        <v-card class="card-review pa-3" max-width="full" height="280">
-          <div
-            v-for="(review, index) in book.reviews
-              .slice()
-              .reverse()
-              .splice(0, 1)"
-            :key="index"
-          >
-            <v-row>
-              <v-col cols="6">
-                <v-card max-width="full" height="250">
-                  <v-row class="ma-2">
+    <div v-if="screenWidth > 500">
+      <v-row class="mb-1">
+        <v-col
+          cols="12"
+          v-for="(book, index) in reviews.slice(
+            (page - 1) * itemsPerPage,
+            page * itemsPerPage
+          )"
+          :key="index"
+          class="mb-5 d-flex justify-center"
+        >
+          <v-card class="card-review pa-3" max-width="full" height="280">
+            <div
+              v-for="(review, index) in book.reviews
+                .slice()
+                .reverse()
+                .splice(0, 1)"
+              :key="index"
+            >
+              <v-row>
+                <v-col cols="6">
+                  <v-card max-width="full" height="250">
+                    <v-row class="ma-2">
+                      <v-col cols="12">
+                        <p class="review-comment">{{ review.comment }}</p>
+                        <p class="text-show-all" @click="hidereview(review)">
+                          แสดงทั้งหมด
+                        </p>
+                      </v-col>
+                      <v-card-subtitle class="subtitle-2 text-uppercase"
+                        >รีวิวจาก
+                      </v-card-subtitle>
+                      <v-col cols="12">
+                        <p>{{ review.user.name }}</p>
+                        <div style="font-size: 10px">
+                          <v-rating
+                            v-model="review.rating"
+                            color="#5a5a5a"
+                            active-color="#e83e8c"
+                            empty-icon="mdi-cards-heart"
+                            full-icon="mdi-cards-heart"
+                            readonly
+                            hover
+                            size="16"
+                          />
+                        </div>
+                      </v-col>
+                    </v-row>
+                  </v-card>
+                </v-col>
+                <v-col cols="6">
+                  <div>
+                    <v-row class="d-flex justify-center">
+                      <v-col cols="12">
+                        <v-img
+                          :src="book.imageBook"
+                          max-width="100%"
+                          height="150"
+                        ></v-img>
+                      </v-col>
+                      <v-col cols="12">
+                        <p
+                          style="font-size: 16px; font-weight: bold"
+                          class="review-comment"
+                        >
+                          {{ book.name }}
+                        </p>
+                        <p style="font-size: 12px">Author: {{ book.author }}</p>
+                        <p style="font-size: 12px">
+                          Category: {{ book.category }}
+                        </p>
+                        <p style="font-size: 12px; color: red">
+                          {{ book.rating }} คะแนน จาก {{ book.ratingsCount }}
+                        </p>
+                      </v-col>
+                    </v-row>
+                  </div>
+                </v-col>
+              </v-row>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-pagination
+        class="text-pagination"
+        v-model="page"
+        :length="pages"
+        circle
+      ></v-pagination>
+    </div>
+    <div v-else>
+      <v-row class="mb-1">
+        <v-col
+          cols="12"
+          v-for="(book, index) in reviews.slice(
+            (page - 1) * itemsPerPage,
+            page * itemsPerPage
+          )"
+          :key="index"
+          class="mb-5"
+        >
+          <v-card class="mx-auto bg-card pa-3" max-width="340" height="360">
+            <div
+              v-for="(review, index) in book.reviews
+                .slice()
+                .reverse()
+                .splice(0, 1)"
+              :key="index"
+            >
+              <v-card max-width="320" height="200">
+                <v-row class="ma-2">
+                  <v-col cols="12">
+                    <p class="review-comment">{{ review.comment }}</p>
+                    <p class="text-show-all" @click="hidereview(review)">
+                      แสดงทั้งหมด
+                    </p>
+                  </v-col>
+                  <v-card-subtitle class="subtitle-2 text-uppercase"
+                    >รีวิวจาก
+                  </v-card-subtitle>
+                  <v-col cols="12">
+                    <p>{{ review.user.name }}</p>
+                    <div style="font-size: 10px">
+                      <v-rating
+                        v-model="review.rating"
+                        color="#5a5a5a"
+                        active-color="#e83e8c"
+                        empty-icon="mdi-cards-heart"
+                        full-icon="mdi-cards-heart"
+                        readonly
+                        hover
+                        size="16"
+                      />
+                    </div>
+                  </v-col>
+                </v-row>
+              </v-card>
+            </div>
+            <v-container>
+              <v-row>
+                <v-col cols="3">
+                  <v-img
+                    :src="book.imageBook"
+                    max-width="60"
+                    height="90"
+                    cover
+                  ></v-img>
+                </v-col>
+                <v-col cols="7">
+                  <v-row>
                     <v-col cols="12">
-                      <p class="review-comment">{{ review.comment }}</p>
-                      <p class="text-show-all" @click="hidereview(review)">
-                        แสดงทั้งหมด
-                      </p>
-                    </v-col>
-                    <v-card-subtitle class="subtitle-2 text-uppercase"
-                      >รีวิวจาก
-                    </v-card-subtitle>
-                    <v-col cols="12">
-                      <p>{{ review.user.name }}</p>
-                      <div style="font-size: 10px">
-                        <v-rating
-                          v-model="review.rating"
-                          color="#5a5a5a"
-                          active-color="#e83e8c"
-                          empty-icon="mdi-cards-heart"
-                          full-icon="mdi-cards-heart"
-                          readonly
-                          hover
-                          size="16"
-                        />
-                      </div>
-                    </v-col>
-                  </v-row>
-                </v-card>
-              </v-col>
-              <v-col cols="6">
-                <div>
-                  <v-row class="d-flex justify-center">
-                    <v-col cols="12">
-                      <v-img
-                        :src="book.imageBook"
-                        max-width="100%"
-                        height="150"
-                      ></v-img>
-                    </v-col>
-                    <!-- <v-col cols="7">
-                      <v-row> -->
-                    <v-col cols="12" >
                       <p
                         style="font-size: 16px; font-weight: bold"
                         class="review-comment"
@@ -87,16 +171,20 @@
                         {{ book.rating }} คะแนน จาก {{ book.ratingsCount }}
                       </p>
                     </v-col>
-                    <!-- </v-row>
-                    </v-col> -->
                   </v-row>
-                </div>
-              </v-col>
-            </v-row>
-          </div>
-        </v-card>
-      </v-col>
-    </v-row>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-pagination
+        class="text-pagination"
+        v-model="page"
+        :length="pages"
+        circle
+      ></v-pagination>
+    </div>
   </div>
 
   <v-dialog v-model="showReview" max-width="600px">
@@ -128,37 +216,37 @@
 
 <script>
 import api from "@/services/api";
+import router from "@/router";
 
 export default {
   data() {
     return {
       reviews: [],
       page: 1,
-      itemsPerPage: 40,
+      itemsPerPage: 10,
       screenWidth: 0,
       showReview: false,
     };
   },
   methods: {
-    async getAllReview() {
-      const res = await api.get("/allreview/books/reviews/");
-      this.reviews = res.data;
-      console.log(this.reviews)
-    },
-    testValue(item) {
-      console.log("test", item);
-    },
     hidereview(review) {
       this.displayName = review.user.name;
       this.userComment = review.comment;
       this.userRating = review.rating;
       this.showReview = true;
     },
+    handleResize() {
+      this.screenWidth = window.innerWidth;
+    },
   },
-  mounted() {
+  async mounted() {
     this.screenWidth = window.innerWidth;
     window.addEventListener("resize", this.handleResize);
-    this.getAllReview();
+    const res = await api.get("/allreview/books/reviews/");
+    this.reviews = res.data;
+    if (this.reviews.length < 3) {
+      router.push("/");
+    }
   },
   computed: {
     pages() {
@@ -170,7 +258,7 @@ export default {
 
 <style scope>
 .card-review {
-  height: 230;
+  height: 230px;
   width: 100%;
   background-color: #f6f6f6;
 }
@@ -193,8 +281,15 @@ export default {
 }
 @media (max-width: 1200px) {
   .card-review {
-    height: 230;
+    height: 230px;
     width: 80%;
+    background-color: #f6f6f6;
+  }
+}
+@media (max-width: 900px) {
+  .card-review {
+    height: 230px;
+    width: 60%;
     background-color: #f6f6f6;
   }
 }
